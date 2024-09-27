12366e42c1111d37a321ca557efe74401705cfff
oleeriksen <oe@easv.dk>
2024-09-27 12:31:31 +0200

    modul7

---
+++ Modul7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Modul7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello modul 7");
+        TestList.Run();
+    }
+}
---
+++ Modul7/TestList.cs
@@ -0,0 +1,30 @@
+namespace Modul7;
+
+public class TestList
+{
+    public static void Run()
+    {
+        List<int> numbers = new();
+        while (true)
+        {
+            int aNumber = ReadInt("Indtast tal: ");
+            if (aNumber == -1)
+                break;
+            numbers.Add(aNumber);
+         
+            Console.WriteLine($"Listen nu: {AsString(numbers)}");
+        }
+
+    }
+
+    private static int ReadInt(string question)
+    {
+        Console.Write(question);
+        return Convert.ToInt32(Console.ReadLine());
+    }
+    
+    private static string AsString(List<int> a)
+    {
+        return $"[{string.Join(", ", a)}]";
+    }
+}
